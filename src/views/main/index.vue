c4df895de3025b7c8b1e03ed54441968c42cb68b
sangryul <ksl970330@naver.com>
2019-10-21 23:49:26 +0900

    add basig routing setting

---
+++ src/views/main/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main-container">
+      <h1> This is main page</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style scoped>
+
+
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -25,6 +25,7 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
   publicPath: '/',
+  lintOnSave: false,
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
