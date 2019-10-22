5594a22f3c70e678c716e1efb9c67f133876b46b
sangryul <ksl970330@naver.com>
2019-10-22 21:02:29 +0900

    change map design

--- src/views/map/map.vue
+++ src/views/map/map.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="main-container">
-    <div class="container">
+  <div class="map-main-container">
+    <div class="list-container">
       <FitCenterList @onFocusCenter="onFocusCenter" />
-      <GoogleMap />
     </div>
-    <transition name="slide-fade">
-      <FitCenterDetail v-if="center !== null" :center="center" />
-    </transition>
+    <div class="map-container">
+      <GoogleMap class="google-map" v-show="!something"/>
+      <FitCenterDetail class="map-detail" v-show="something" />
+    </div>
+      
   </div>
+
 </template>
 
 <script>
@@ -19,30 +21,68 @@ export default {
   components: { FitCenterList, GoogleMap, FitCenterDetail },
   data() {
     return {
-      center: null
+      center: null,
+      something : false
     }
   },
   methods: {
     onFocusCenter: function(c) {
       this.center = c
+      this.something = !this.something
+      console.log("dfdfdf")
+      console.log(this.something)
     }
   }
 }
 </script>
-<style>
-.fit-center-list {
-  width: 300px;
-  height: 100%;
+<style lang="scss" scoped>
+.map-main-container{
+  display : flex;
+  margin : 0;
+  padding : 0;
+  width : 100%;
+  min-height: calc(100vh - 84px);
+
+  .list-container{
+    border : 1px solid red;
+    width: 300px;
+    min-height: 100%;
+
+  }
+  .map-container{
+    min-height : 100%;
+    position : relative;
+    border : 1px solid blue;
+    width : calc(100% - 300px);
+
+    .map-detail{
+      position : relative;
+      z-index:999999 !important;
+      width : 100%;
+      height : 100%;
+    }
+
+  }
 }
-.google-map {
-  width: 100%;
-  height: 100%;
-}
+// .fit-center-list {
+//   border : 1px solid red;
+//   width: 300px;
+//   height: 100%;
+// }
+// .google-map, .google-map-detail {
+//   width: 100%;
+//   height: calc(100vh - 80px);
+// }
+// .google-map-detail{
+//   position : absolute;
+//   padding : 10px;
+//   border : 1px solid red;
+// }
 /* .main-container {
   height: 1000px;
 } */
-.container {
-  display: flex;
-}
+// .container {
+//   display: flex;
+// }
 </style>
 
