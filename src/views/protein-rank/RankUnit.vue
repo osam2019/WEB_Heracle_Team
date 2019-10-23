c485eca47d35f2d4da958d19b82aa95395f331de
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-10-23 15:14:05 +0900

    Change product rank page

---
+++ src/views/protein-rank/RankUnit.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="unit-container">
+    <el-row class="unit-header">
+      <el-col :span="20">
+        <div>
+          <p>
+            <b>{{ data.subtitle }}</b>
+          </p>
+          <p>{{ data.title }}</p>
+        </div>
+      </el-col>
+      <el-col :span="1">
+        <div>
+          <el-button icon="el-icon-search" circle />
+        </div>
+      </el-col>
+    </el-row>
+    <div class="unit-body">
+      <ul>
+        <li v-for="(product, idx) of data.ranker" :key="product.id" class="unit-li">
+          <RankUnitItem :product="product" :idx="idx" />
+          <hr>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+import RankUnitItem from './RankUnitItem'
+
+export default {
+  components: {
+    RankUnitItem
+  },
+  props: ['data']
+}
+</script>
+<style scoped>
+.unit-header {
+  background: greenyellow;
+  padding: 5px;
+}
+.unit-li {
+  list-style-type: none;
+}
+</style>
---
+++ src/views/protein-rank/RankUnitItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-row class="item-container">
+    <el-col :span="3">
+      <img :src="medalIcons[idx]">
+    </el-col>
+    <el-col :span="3">
+      <img class="product-img" :src="product.img">
+    </el-col>
+    <el-col :span="3" class="product-info-container">
+      <p>{{ product.brand }}</p>
+      <p>{{ product.name }}</p>
+    </el-col>
+  </el-row>
+</template>
+<script>
+import MedalFirst from '@/assets/image/medal-first.png'
+import MedalSecond from '@/assets/image/medal-second.png'
+import MedalThird from '@/assets/image/medal-third.png'
+
+export default {
+  props: ['product', 'idx'],
+  data() {
+    return {
+      medalIcons: [MedalFirst, MedalSecond, MedalThird]
+    }
+  }
+}
+</script>
+<style>
+.item-container {
+}
+.product-img {
+  width: 100px;
+  height: 100px;
+  margin: 10px;
+}
+.product-info-container {
+}
+</style>
