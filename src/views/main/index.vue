9d8e9521ab696c4cc773a1575ec596d00a1cdd66
sangryul <ksl970330@naver.com>
2019-10-23 17:42:07 +0900

    change minimal design

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -146,9 +146,6 @@ export default {
     .center-main{
       .logo-button-router{
         width : 100%;
-        height : 150px;
-        box-shadow: 4px 4px 40px rgba(0, 0, 0, .05);
-        border-color: rgba(0, 0, 0, .05);
       }
       .card-view-container{
         margin-top : 20px;
